bfaa36b6f7ad092f3f334b5df353c226a887d2f9
balintnyirfa <78541351+balintnyirfa@users.noreply.github.com>
2021-02-10 13:41:10 +0100

    Add files via upload
    Kezdeti projekt feltöltve!

---
+++ kozos_projekt.py
@@ -0,0 +1,7 @@
+#Modulok meghívása
+import filebeolvasas
+import elegtelen_osztalyzat
+import atlag
+
+#Felhasználü tájékoztatása
+print("Ez a program egy txt fájlból olvassa be az adatokat és eltárolja azokat. Ezekkel az adatokkal statisztikát számol és megjeleníti az eredményeket.")
